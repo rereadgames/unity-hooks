d77d467ab80f630aa4a30b01604d85c7f7911af3
Matthew Padbury <mpforce1@gmail.com>
2016-09-06 12:19:20 +0100

    Updated install-hooks.bat to use file symbolic links

--- install-hooks.bat
+++ install-hooks.bat
@@ -1,10 +1,12 @@
-@echo off
+
 if not exist ".git" (
     echo Not in a git repository
     exit /b 1
 )
 
-FOR %%A IN (pre-commit post-checkout post-merge) DO mklink /d "%cd%\.git\hooks\%A" "%~dp0delegate.py"
+for %%A in (pre-commit post-checkout post-merge) do (
+	mklink "%cd%\.git\hooks\%%A" "%~dp0delegate.py"
+)
 
 if %errorlevel%==0 (
 	echo Installed Unity hooks.
