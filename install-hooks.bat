c520539edc07d06234273860808cc3d921369e31
Matthew Padbury <mpforce1@gmail.com>
2016-09-06 12:34:45 +0100

    fixed directory issue

--- install-hooks.bat
+++ install-hooks.bat
@@ -1,11 +1,11 @@
-
+@echo off
 if not exist ".git" (
     echo Not in a git repository
     exit /b 1
 )
 
 for %%A in (pre-commit post-checkout post-merge) do (
-	mklink "%cd%\.git\hooks\%%A" "%~dp0delegate.py"
+	mklink "%cd%\.git\hooks\%%A" "%~dp0hooks\delegate.py"
 )
 
 if %errorlevel%==0 (
